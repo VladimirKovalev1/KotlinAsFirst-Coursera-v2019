fc4134b91e1e511bbc052457ea18ee7c248d78f7
VladimirKovalev1 <deltacat@mail.ru>
2019-12-03 10:30:31 +0700

    Решены задачи thirdDigit и travelMinutes  урока 1

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -49,10 +49,9 @@ fun quadraticRootProduct(a: Double, b: Double, c: Double): Double {
  * Пример главной функции
  */
 fun main() {
-    //val x1x2 = quadraticRootProduct(1.0, 13.0, 42.0)
-   // println("Root product: $x1x2")
-    //val result =sqr(7)
-    println("7*7 = " +7*7)
+
+    val result = thirdDigit(3801)
+    println("Result: $result")
 }
 
 /**
